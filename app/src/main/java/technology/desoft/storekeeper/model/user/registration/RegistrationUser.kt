5c63ff0043a4fb4a222364c248acc25f3ce5b16c
dliubitsky <d.ljubickij@desoft.technology>
2019-02-21 20:14:05 +0600

    fix regsitration bug

--- app/src/main/java/technology/desoft/storekeeper/model/user/registration/RegistrationUser.kt
+++ app/src/main/java/technology/desoft/storekeeper/model/user/registration/RegistrationUser.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class RegistrationUser(
     val email: String,
     val password: String,
+    @SerializedName("name")
     val username: String,
     @SerializedName("is_keeper")
     val isKeeper: Boolean
