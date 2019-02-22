7c2352dffbeef4af5489761d9c0686105b706f5e
daniilBugay <d.bugaj@desoft.technology>
2019-02-22 17:53:10 +0600

    fix bug with timeout exception

--- app/src/main/java/technology/desoft/storekeeper/model/user/retrofit/RetrofitUserRepository.kt
+++ app/src/main/java/technology/desoft/storekeeper/model/user/retrofit/RetrofitUserRepository.kt
@@ -9,26 +9,46 @@ import technology.desoft.storekeeper.model.user.login.LoginException
 import technology.desoft.storekeeper.model.user.login.LoginUser
 import technology.desoft.storekeeper.model.user.registration.RegistrationException
 import technology.desoft.storekeeper.model.user.registration.RegistrationUser
+import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
 class RetrofitUserRepository(retrofit: Retrofit, private val resources: Resources) : UserRepository {
     private val api = retrofit.create(UserApi::class.java)
 
+    private companion object {
+        const val REPEAT_COUNT = 3
+    }
+
     override suspend fun login(loginUser: LoginUser): AuthResult {
+        return loginWithRepeating(loginUser, 3)
+    }
+
+    private suspend fun loginWithRepeating(loginUser: LoginUser, repeatCount: Int): AuthResult {
         try {
             if (loginUser.email.isBlank() || loginUser.password.isBlank())
                 throw LoginException(resources.getString(R.string.fields_empty_error))
+
             val response = api.login(loginUser).await()
             val body = response.body()
             if (response.isSuccessful && body != null)
                 return body
+
             throw LoginException(resources.getString(R.string.invalid_login_error))
+        } catch (e: SocketTimeoutException) {
+            if (repeatCount > 0)
+                return loginWithRepeating(loginUser, repeatCount - 1)
+
+            throw LoginException(resources.getString(R.string.connection_error))
         } catch (e: UnknownHostException) {
             throw LoginException(resources.getString(R.string.connection_error))
         }
     }
 
     override suspend fun registration(registrationUser: RegistrationUser): AuthResult {
+        return registerWithRepeating(registrationUser, REPEAT_COUNT)
+    }
+
+    private suspend fun registerWithRepeating(registrationUser: RegistrationUser, repeatCount: Int): AuthResult {
         try {
             if (
                 registrationUser.email.isBlank() ||
@@ -40,9 +60,17 @@ class RetrofitUserRepository(retrofit: Retrofit, private val resources: Resource
             val body = response.body()
             if (response.isSuccessful && body != null)
                 return body
+
             throw RegistrationException(resources.getString(R.string.registration_error))
+        } catch (e: SocketTimeoutException) {
+            if (repeatCount > 0)
+                return registerWithRepeating(registrationUser, repeatCount - 1)
+
+            throw RegistrationException(resources.getString(R.string.connection_error))
         } catch (e: UnknownHostException) {
             throw RegistrationException(resources.getString(R.string.connection_error))
         }
     }
+
+
 }
